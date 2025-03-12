95878c94c214f94472f84d22a5d6854f9a113eb2
taylfw <frank.w.taylor88@gmail.com>
2025-03-11 22:23:29 -0400

    changed some variables.

--- doNotRun.sh
+++ doNotRun.sh
@@ -3,16 +3,16 @@
 # This script will append "Chaos!" to output.txt until the file size is greater than the free space in /var/log.
 # The purpose of this script is to test alerting on system crashes.
 
-
 CHAOS="Chaos!"
 free_space=$(df --output=avail /var/log | tail -n 1)
-file_size=$(stat -c%s "output.txt")
+file_size=$(stat -c%s "/var/log/output.txt")
 
 while [ $file_size -le $free_space ]
 
 do
-
+  echo $CHAOS
+  echo "File Size: $file_size"
+  echo "Free Space: $free_space"
   echo $CHAOS >> /var/log/output.txt
 
 done
-
